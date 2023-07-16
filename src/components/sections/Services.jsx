45b06bd4c03521b517819fbaf5e7b9f2cb60e15e
NetHunter123 <viktor3@i.ua>
2023-07-16 22:29:41 +0300

    before make updates of contact

--- src/components/sections/Services.jsx
+++ src/components/sections/Services.jsx
@@ -6,9 +6,9 @@ const Services = () => {
     <div className={"container"}>
       <a id={"services"} className="anchor"/>
       <div className="z-10 py-[40px]">
-        <h3 className="title">Services</h3>
-        <p className="sub-title">The official website of Julia Koppel invites you to familiarize yourself with the
-          services provided:</p>
+        <h3 className="title">Услуги</h3>
+        <p className="sub-title">Здесь можно ознакомиться с предоставляемыми услугами от Анны Астаховой:
+        </p>
         <div className="max-w-[760px] mx-auto">
           <AccordionServices/>
         </div>
